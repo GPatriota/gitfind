19f7f5ed27250c1e1fc1eb59a90318c6f900baa8
GPatriota <gpspatriota@gmail.com>
2025-01-23 11:05:31 -0300

    Project Finished GitFind

--- src/Pages/Home/App.js
+++ src/Pages/Home/App.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
 import background from "../../assets/background.png";
+import ItemList from "../../components/ItemList";
 import "./styles.css";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json;
+
+    if (newUser.name) {
+      const { avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login });
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json;
+      console.log(repos);
+      if(newRepos.length){
+        setRepos(newRepos);
+      }
+    }
+  };
+
   return (
     <div className="App">
       <Header />
@@ -10,18 +33,39 @@ function App() {
         <img src={background} className="background" alt="Tela de Fundo" />
         <div className="info">
           <div>
-            <input name="user" placeholder="@username" />
-            <button>Buscar</button>
+            <input
+              name="user"
+              value={user}
+              onChange={(event) => setUser(event.target.value)}
+              placeholder="@username"
+            />
+            <button onClick={handleGetData}>Buscar</button>
           </div>
-          <div className="profile">
-            <img src="https://avatars.githubusercontent.com/u/155376842?v=4" alt="imagem de perfil"className="profileimg"/>
+          {currentUser?.name ? (<>
+            <div className="profile">
+            <img
+              src={currentUser.avatar_url}
+              alt="imagem de perfil"
+              className="profileimg"
+            />
             <div>
-              <h3>Gustavo Patriota Silva</h3>
-              <span>@GPatriota</span>
-              <p>Descrição do github</p>
+              <h3>{currentUser.name}</h3>
+              <span>{currentUser.login}</span>
+              <p>{currentUser.bio}</p>
             </div>
           </div>
           <hr />
+          </>): null}
+          {repos?.length ? (<>
+            <div>
+            <h4 className="repositorie"> Repositórios </h4>
+            {repos.map(repo=>(
+              <ItemList title={repo.name} description={repo.description} />
+            )
+            )}
+            
+          </div>
+          </>): null}
         </div>
       </div>
     </div>
